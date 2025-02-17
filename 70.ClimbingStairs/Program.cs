e925f5ce3ad1ff76add96da74551b47fc85c5ac5
Little Soft <a0919459151@gmail.com>
2025-02-18 01:44:38 +0800

    feat : Climb Stairs

---
+++ 70.ClimbingStairs/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace _70.ClimbingStairs;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Test1: {Test1()}");
+        Console.WriteLine($"Test2: {Test2()}");
+    }
+
+    private static string Test1()
+    {
+        var input = 2;
+
+        var expect = 2;
+
+        var ans = Solution.ClimbStairs(input);
+
+        return ans == expect
+            ? "success"
+            : "fail";
+    }
+
+    private static string Test2()
+    {
+        var input = 3;
+
+        var expect = 3;
+
+        var ans = Solution.ClimbStairs(input);
+
+        return ans == expect
+            ? "success"
+            : "fail";
+    }
+}
+
+
+public class Solution
+{
+    public static int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+    }
+}
