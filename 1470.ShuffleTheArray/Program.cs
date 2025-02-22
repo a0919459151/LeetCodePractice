aa8869be9cce1f0e6ac89905806b94c2c9c360ed
Little Soft <a0919459151@gmail.com>
2025-02-22 12:39:39 +0800

    feat : Shuffle The Array

--- 1470.ShuffleTheArray/Program.cs
+++ 1470.ShuffleTheArray/Program.cs
@@ -6,4 +6,18 @@ public class Program
     {
         Console.WriteLine($"Test1: {Test1()}");
     }
+
+    private static string Test1()
+    {
+        Solution solution = new();
+
+        int[] nums = [2, 5, 1, 3, 4, 7];
+        var n = 3;
+
+        int[] expect = [2, 3, 5, 4, 1, 7];
+
+        var ans = solution.Shuffle(nums, n);
+
+        return ans.SequenceEqual(expect) ? "success" : "fail";
+    }
 }
--- 1470.ShuffleTheArray/Solution.cs
+++ 1470.ShuffleTheArray/Solution.cs
@@ -2,8 +2,25 @@
 
 public class Solution
 {
+    /// <summary>
+    /// 洗牌
+    /// </summary>
+    /// <param name="nums">排堆</param>
+    /// <param name="n">分組的大小</param>
+    /// <returns></returns>
     public int[] Shuffle(int[] nums, int n)
     {
+        List<int> result = new(nums.Length);
 
+        Span<int> firstHalf = nums.AsSpan(0, n);
+        Span<int> secondHalf = nums.AsSpan(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            result.Add(firstHalf[i]);
+            result.Add(secondHalf[i]);
+        }
+
+        return result.ToArray();
     }
 }
