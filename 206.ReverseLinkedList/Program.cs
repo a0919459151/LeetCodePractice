386397908e3aa14226c55ce37016a6b18ff8f082
Little Soft <a0919459151@gmail.com>
2025-03-01 23:23:51 +0800

    feat : reverse linked list

--- 206.ReverseLinkedList/Program.cs
+++ 206.ReverseLinkedList/Program.cs
@@ -12,14 +12,14 @@ class Program
     private static string Test1()
     {
         Solution solution = new();
-
+        
         // 建立測試用的鏈表 [1,2,3,4,5]
-        ListNode head = new(1, new(2, new(3, new(4, new(5, null)))));
+        ListNode? head = new(1, new(2, new(3, new(4, new(5, null)))));
     
         // 預期結果應該是 [5,4,3,2,1]
-        ListNode expected = new(5, new(4, new(3, new(2, new(1, null)))));
+        ListNode? expected = new(5, new(4, new(3, new(2, new(1, null)))));
     
-        ListNode ans = solution.ReverseList(head); // 執行反轉
+        ListNode? ans = solution.ReverseList(head); // 執行反轉
     
         // 驗證結果是否正確
         return CompareLinkedLists(ans, expected) ? "success" : "fail";
@@ -33,7 +33,7 @@ class Program
     
         ListNode? expected = null;
     
-        ListNode ans = solution.ReverseList(head); // 執行反轉
+        ListNode? ans = solution.ReverseList(head); // 執行反轉
     
         // 驗證結果是否正確
         return CompareLinkedLists(ans, expected) ? "success" : "fail";
@@ -47,20 +47,23 @@ class Program
     
         ListNode? expected = new(1);
     
-        ListNode ans = solution.ReverseList(head); // 執行反轉
+        ListNode? ans = solution.ReverseList(head); // 執行反轉
     
         // 驗證結果是否正確
         return CompareLinkedLists(ans, expected) ? "success" : "fail";
     }
 
-    private static bool CompareLinkedLists(ListNode l1, ListNode l2)
+    private static bool CompareLinkedLists(ListNode? l1, ListNode? l2)
     {
-        while (l1 != null && l2 != null) 
+        while (l1 != null && l2 != null)
         {
             if (l1.Val != l2.Val) return false;
+
             l1 = l1.Next;
             l2 = l2.Next;
         }
+
         return l1 == null && l2 == null;
     }
+
 }
--- 206.ReverseLinkedList/Solution.cs
+++ 206.ReverseLinkedList/Solution.cs
@@ -5,26 +5,27 @@ public class Solution
     public ListNode? ReverseList(ListNode? head)
     {
         if (head is null) return null;
-        if (head.Next is null) return head;
 
-        ListNode prev = null!;
+        ListNode? prev = null;
         ListNode current = head;
-        ListNode? nextTemp = head.Next;
-        
-        // Try next until next node is null
-        while (current != null)
+
+        while (true)
         {
+            // Store next node
+            ListNode? nextTemp = current.Next;
+
             // Reverse current next node
             current.Next = prev;
+
+            // If next node is null, return current node
+            if (nextTemp is null)
+            {
+                return current;
+            }
 
             // Next round
             prev = current;
             current = nextTemp;
-
-            if (current is null)
-            {
-                return current;
-            }
         }
     }
 }
