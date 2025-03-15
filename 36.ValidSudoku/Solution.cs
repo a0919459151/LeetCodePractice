4918e8a8b343ac495163db05b2c10fe3f83d6727
Little Soft <a0919459151@gmail.com>
2025-03-16 00:15:55 +0800

    feat : valid sudoku

--- 36.ValidSudoku/Solution.cs
+++ 36.ValidSudoku/Solution.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _36.ValidSudoku
+﻿namespace _36.ValidSudoku
 {
     public class Solution
     {
@@ -57,7 +51,6 @@ namespace _36.ValidSudoku
             }
 
             existingSet.Add(key);
-
             return true;
         }
 
@@ -72,7 +65,6 @@ namespace _36.ValidSudoku
             }
 
             existingSet.Add(key);
-
             return true;
         }
 
@@ -90,7 +82,6 @@ namespace _36.ValidSudoku
             }
 
             existingSet.Add(key);
-
             return true;
         }
     }
