3bf37b73d409a18ec14567a1d2631770d35c2292
Little Soft <a0919459151@gmail.com>
2025-03-23 13:17:48 +0800

    feat : 424.LongestRepeatingCharacterReplacement

---
+++ LeetCodePractice.Test/LongestRepeatingCharacterReplacementTest.cs
@@ -0,0 +1,38 @@
+﻿using LeetCodePractice.LongestRepeatingCharacterReplacement;
+
+namespace LeetCodePractice.Test;
+
+public class LongestRepeatingCharacterReplacementTest
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var input = "ABAB";
+        var k = 2;
+        var expected = 4;
+
+        // Act
+        LongestRepeatingCharacterReplacement_Solution solution = new();
+        var actual = solution.CharacterReplacement(input, k);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var input = "AABABBA";
+        var k = 1;
+        var expected = 4;
+
+        // Act
+        LongestRepeatingCharacterReplacement_Solution solution = new();
+        var actual = solution.CharacterReplacement(input, k);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ LeetCodePractice/SlidingWindow/424.LongestRepeatingCharacterReplacement/LongestRepeatingCharacterReplacement_Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodePractice.LongestRepeatingCharacterReplacement;
+
+public class LongestRepeatingCharacterReplacement_Solution
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        var maxWindowLength = 0;
+        var letterCountArr = new int[26];
+
+        var left = 0;
+        var right = 0;
+
+        for (; right < s.Length; right++)
+        {
+            var rightLetterPosition = GetLetterPosition(s[right]);
+            letterCountArr[rightLetterPosition] += 1;
+
+            var countMax = letterCountArr.Max();
+            var currentWindowLength = right - left + 1;
+
+            var isInValidSubstring = currentWindowLength - countMax > k;
+
+            // 如果超出 k 的用量
+            if (isInValidSubstring)
+            {
+                // 對應 letter count 減一
+                var leftLetterPosition = GetLetterPosition(s[left]);
+                letterCountArr[leftLetterPosition] -= 1;
+
+                // 右移 left 指標
+                left += 1;
+
+                // 重算 currentWindowLength
+                currentWindowLength = (right - left + 1);
+            }
+
+            // 紀錄最大窗口長度
+            maxWindowLength = Math.Max(maxWindowLength, currentWindowLength);
+        }
+
+        return maxWindowLength;
+    }
+
+    // Get Position
+    private int GetLetterPosition(char letter)
+    {
+        // A-Z is 0-25
+        return char.ToUpper(letter) - 'A';
+    }
+}
