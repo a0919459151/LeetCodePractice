71f2f0ea01fcddad8697945bec0333575c8f7197
Little Soft <a0919459151@gmail.com>
2025-03-23 13:45:50 +0800

    feat : LongestRepeatingCharacterReplacement_Solution

--- LeetCodePractice/SlidingWindow/424.LongestRepeatingCharacterReplacement/LongestRepeatingCharacterReplacement_Solution.cs
+++ LeetCodePractice/SlidingWindow/424.LongestRepeatingCharacterReplacement/LongestRepeatingCharacterReplacement_Solution.cs
@@ -6,45 +6,67 @@ public class LongestRepeatingCharacterReplacement_Solution
     {
         var maxWindowLength = 0;
         var letterCountArr = new int[26];
-
         var left = 0;
         var right = 0;
 
         for (; right < s.Length; right++)
         {
-            var rightLetterPosition = GetLetterPosition(s[right]);
-            letterCountArr[rightLetterPosition] += 1;
+            var rightLetter = s[right];
+            IncreaseLetterCount(letterCountArr, rightLetter);
 
-            var countMax = letterCountArr.Max();
-            var currentWindowLength = right - left + 1;
-
-            var isInValidSubstring = currentWindowLength - countMax > k;
+            bool isInValidSubstring = IsInvalidSubstring(k, letterCountArr, left, right);
 
             // 如果超出 k 的用量
             if (isInValidSubstring)
             {
                 // 對應 letter count 減一
-                var leftLetterPosition = GetLetterPosition(s[left]);
-                letterCountArr[leftLetterPosition] -= 1;
+                var leftLetter = s[left];
+                DecreaseLetterCount(letterCountArr, leftLetter);
 
                 // 右移 left 指標
                 left += 1;
-
-                // 重算 currentWindowLength
-                currentWindowLength = (right - left + 1);
             }
 
             // 紀錄最大窗口長度
-            maxWindowLength = Math.Max(maxWindowLength, currentWindowLength);
+            maxWindowLength = Math.Max(maxWindowLength, CalWindowLength(left, right));
         }
 
         return maxWindowLength;
     }
 
-    // Get Position
-    private int GetLetterPosition(char letter)
+    // Is Invalid Substring
+    private static bool IsInvalidSubstring(int k, int[] letterCountArr, int left, int right)
+    {
+        var countMax = letterCountArr.Max();
+        var currentWindowLength = CalWindowLength(left, right);
+
+        return currentWindowLength - countMax > k;
+    }
+
+    // Increase letter count
+    private static void IncreaseLetterCount(int[] letterCountArr, char letter)
+    {
+        var letterPosition = GetLetterPosition(letter);
+        letterCountArr[letterPosition] += 1;
+    }
+
+    // Decrease
+    private static void DecreaseLetterCount(int[] letterCountArr, char letter)
+    {
+        var letterPosition = GetLetterPosition(letter);
+        letterCountArr[letterPosition] -= 1;
+    }
+
+    // Get letter position
+    private static int GetLetterPosition(char letter)
     {
         // A-Z is 0-25
         return char.ToUpper(letter) - 'A';
+    }
+
+    // Calculate window length
+    private static int CalWindowLength(int left, int right)
+    {
+        return right - left + 1;
     }
 }
